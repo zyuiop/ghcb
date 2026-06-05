273f0f6c8488405a86507ce71dfd4882333a215c
Louis Vialar <louis.vialar@gmail.com>
2026-06-05 11:29:20 +0200

    change allocation API

--- src/mapping.rs
+++ src/mapping.rs
@@ -1,20 +1,89 @@
-use crate::msr::GhcbMsr;
-use crate::msr::page_state_change::{PageStateChangeRequest, PageStateOperation};
 use crate::ptr::OwnedPtrWithPhysAddr;
 use core::mem::MaybeUninit;
 use x86_64::VirtAddr;
 use x86_64::structures::paging::{PageSize, PageTableFlags, PhysFrame, Size4KiB};
+
+pub mod mapping_utils {
+    use x86_64::structures::paging::{PhysFrame, Size4KiB};
+    use x86_64::VirtAddr;
+    use crate::instructions::pvalidate::pvalidate;
+    use crate::msr::GhcbMsr;
+    use crate::msr::page_state_change::{PageStateChangeRequest, PageStateOperation};
+    use crate::protocols::change_page_state::PageStateChangePageSize;
+
+    /// Makes a frame shared with the hypervisor.
+    ///
+    /// # Safety
+    ///
+    /// virt_addr must map to the provided frame
+    pub unsafe fn make_shared(frame: PhysFrame<Size4KiB>, virt_addr: VirtAddr) {
+        // Rescind RMP validation for page
+        pvalidate(PageStateChangePageSize::PageSize4KB, false, virt_addr);
+
+        // Tell the hypervisor to make the frame shared
+        let result = unsafe {
+            GhcbMsr::execute(PageStateChangeRequest::create(
+                frame,
+                PageStateOperation::AssignShared,
+            ))
+        };
+
+        if !result.is_successful() {
+            panic!(
+                "failed to share page with hypervisor: {}",
+                result.0.unwrap()
+            )
+        }
+    }
+
+    /// Makes a frame non shared with the hypervisor.
+    ///
+    /// # Safety
+    ///
+    /// virt_addr must map to the provided frame
+    pub unsafe fn make_private(frame: PhysFrame<Size4KiB>, virt_addr: VirtAddr) {
+        // Tell the hypervisor to make the frame private
+        let result = unsafe {
+            GhcbMsr::execute(PageStateChangeRequest::create(
+                frame,
+                PageStateOperation::AssignPrivate,
+            ))
+        };
+
+        if !result.is_successful() {
+            panic!(
+                "failed to share page with hypervisor: {}",
+                result.0.unwrap()
+            )
+        }
+
+        // Grant validation for page
+        pvalidate(PageStateChangePageSize::PageSize4KB, true, virt_addr);
+    }
+}
 
 /// # Safety
 ///
 /// Similar to [core::alloc::Allocator]
 pub unsafe trait PhysicalAllocator {
     /// Allocates a 4KiB frame and maps it with the provided page flags.
-    fn allocate(flags: PageTableFlags) -> Option<(PhysFrame<Size4KiB>, VirtAddr)>;
-
+    ///
     /// # Safety
     ///
-    /// The memory must have been allocated by this allocator
+    /// If [PageTableFlags::is_encrypted] is false, the allocated frame must have been shared with
+    /// the hypervisor. Implementors can use [mapping_utils::make_shared] for that.
+    ///
+    /// Allocators must zero the memory before returning it.
+    fn allocate_zeroed(flags: PageTableFlags) -> Option<(PhysFrame<Size4KiB>, VirtAddr)>;
+
+    /// Deallocates the 4KB frame starting at given address.
+    ///
+    /// # Safety
+    ///
+    /// The memory must have been allocated by this allocator.
+    ///
+    /// If implementors return the frame to a usable pool, they must ensure it is set to private
+    /// again. They can use [mapping_utils::make_private] for that.
     unsafe fn deallocate(addr: VirtAddr);
 
     /// Allocates a new frame and page, zeroes it, and wraps it in an OwnedPtr
@@ -26,32 +95,9 @@ pub unsafe trait PhysicalAllocator {
             "object too big to allocate"
         );
 
-        let (frame, virt) = Self::allocate(flags)?;
-
-        unsafe {
-            // zeroise allocated memory
-            virt.as_mut_ptr::<T>().write_bytes(0, 1);
-        }
-
-        if !flags.is_encrypted() {
-            // Tell the hypervisor to make the frame shared
-            let result = unsafe {
-                GhcbMsr::execute(PageStateChangeRequest::create(
-                    frame,
-                    PageStateOperation::AssignShared,
-                ))
-            };
-
-            if !result.is_successful() {
-                panic!(
-                    "failed to share page with hypervisor: {}",
-                    result.0.unwrap()
-                )
-            }
-        }
-
+        let (frame, virt) = Self::allocate_zeroed(flags)?;
         Some(unsafe {
-            // SAFETY: we have verified the
+            // SAFETY: memory is zeroed
             OwnedPtrWithPhysAddr::<_, Self>::from_alloc(virt, frame.start_address())
         })
     }
